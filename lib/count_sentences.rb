3d869168eb16cef6c96566134331d6094b39c27a
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-09 19:06:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,20 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence?(str)
+    str.end_with?(".")
   end
 
-  def question?
-
+  def question?(str)
+    str.end_with?("?")
   end
 
-  def exclamation?
-
+  def exclamation?(str)
+    str.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences(str)
+    words = str.split
+    words.count
   end
 end
