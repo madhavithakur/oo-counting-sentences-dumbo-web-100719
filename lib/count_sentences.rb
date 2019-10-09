4f1233fab183d9f5d2c4a0d2c46b416bf9d3e5f5
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-09 19:24:48 +0000

    counting_sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class String
+  
+  #attr_accessor
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    words = self.split.select {|word| word.end_with?(".", "!", "?")}
+    words.count
   end
 end
